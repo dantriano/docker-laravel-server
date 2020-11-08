cb544ef9565507eb79f2aa9adaa1539807af353a
Dan <dan.triano@gmail.com>
2020-11-08 13:52:20 +0100

    Created user/db for Mysql

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,4 +1,5 @@
-mysql -u root -p password;
 show databases;
-GRANT ALL ON laravel.* TO 'laravel'@'%' IDENTIFIED BY 'password';
+CREATE DATABASE IF NOT EXISTS laravel;
+CREATE USER  IF NOT EXISTS 'laravel'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON laravel.* TO 'laravel'@'%';
 FLUSH PRIVILEGES;
